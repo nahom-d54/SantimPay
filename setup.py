11d6c7c65a56c1ba5a3bddc14204965d01457b72
Nahom_d54 <nahom.d54@gmail.com>
2024-05-02 22:14:52 +0300

    feat: upgraded setup file to v 1.0.1

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'SantimPay Unofficial package'
-LONG_DESCRIPTION = 'Santim pay python package which is directly made from the php laravel package https://github.com/ba5liel/santimpay-laravel/tree/main'
+LONG_DESCRIPTION = ''
+with open('README.md', 'r') as r:
+    LONG_DESCRIPTION = r.read()
 
 setup(
     name='santimPay',
     version=VERSION,
     author='Nahom Dereje',
+    license='MIT',
     author_email='dev@nahom.eu.org',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     url="https://github.com/nahom-d54/SantimPay.git",
     install_requires=[
