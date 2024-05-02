ec913fa32a4499afc3d8c50fce7fb58408c35f1b
Nahom_d54 <nahom.d54@gmail.com>
2024-05-02 21:41:33 +0300

    feat: added config to avoid circular import error

---
+++ santimPay/config.py
@@ -0,0 +1,5 @@
+class Config:
+    DEFAULT_HOST = 'https://services.santimpay.com'
+    DEFAULT_TEST_HOST = 'https://testnet.santimpay.com'
+    API_VERSION = '/api/v1/gateway'
+    PACKAGE_VERSION = '1.0.0'
